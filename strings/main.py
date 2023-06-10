8690ac9ce6dab7bf0426011e230d92bbd9149361
Ani050 <132291452+Ani050@users.noreply.github.com>
2023-06-10 17:58:45 +0200

    Add files via upload
    I want my feedback in English

---
+++ strings/main.py
@@ -0,0 +1,26 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+player0 = "Ruud Gullit"
+player1 = "Marco van Basten"
+goal_0 = 32
+goal_1 = 54
+scorer= "Ruud Gullit"
+when_he_scored0 = 32
+scorer1 = "Marco van Basten"
+when_he_scored1 = 54
+scorers= "Ruud Gullit 32, Marco van Basten 54"
+print(f"{scorer} {when_he_scored0}, {scorer1} {when_he_scored1}")
+report = f"{player0} scored in the {goal_0}nd minute\n{player1} scored in the {goal_1}th minute"
+player= "Ruud Gullit"
+first_name = "Ruud"
+last_name_len = 6
+name_short = "R. Gullit"
+chant = "Ruud! Ruud! Ruud! Ruud!"
+good_chant = (2!=3)
+
+
+
+
